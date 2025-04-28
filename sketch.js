8aaa7f0c710414b8740158a02974c9eef7b4397f
Kulami777 <singying941213@gmail.com>
2025-04-28 09:58:10 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+let capture;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background('#fdf0d5');
+  
+  // 初始化攝影機
+  capture = createCapture(VIDEO);
+  capture.size(windowWidth * 0.8, windowHeight * 0.8);
+  capture.hide(); // 隱藏原始的 HTML 視訊元素
+}
+
+function draw() {
+  background('#fdf0d5');
+  
+  // 計算影像顯示位置，使其居中
+  let x = (windowWidth - capture.width) / 2;
+  let y = (windowHeight - capture.height) / 2;
+  
+  // 繪製攝影機影像
+  image(capture, x, y, capture.width, capture.height);
+}
+
+function windowResized() {
+  // 當視窗大小改變時，調整畫布大小
+  resizeCanvas(windowWidth, windowHeight);
+}
